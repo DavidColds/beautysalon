43332cf7c8b3d911197d1a6cb0b1616a1a375f73
David Drughi <drughidavid6@gmail.com>
2020-03-02 17:00:53 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".fixed-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      });
+  });
+
+
+  $(function() {
+  
+    // contact form animations
+    $('#contact').click(function() {
+      $('#contactForm').fadeToggle();
+    })
+    $(document).mouseup(function (e) {
+      var container = $("#contactForm");
+  
+      if (!container.is(e.target) // if the target of the click isn't the container...
+          && container.has(e.target).length === 0) // ... nor a descendant of the container
+      {
+          container.fadeOut();
+      }
+    });
+    
+  });
