fdf8d0ae82181cafb7ad4f7f6749afffd8be488e
David Drughi <drughidavid6@gmail.com>
2020-03-03 09:11:43 +0100

    the carousel is working perfectly

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,31 @@
 $(function () {
-    $(document).scroll(function () {
-        var $nav = $(".fixed-top");
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-      });
-  });
-
-
-  $(function() {
-  
-    // contact form animations
-    $('#contact').click(function() {
-      $('#contactForm').fadeToggle();
-    })
-    $(document).mouseup(function (e) {
-      var container = $("#contactForm");
-  
-      if (!container.is(e.target) // if the target of the click isn't the container...
-          && container.has(e.target).length === 0) // ... nor a descendant of the container
-      {
-          container.fadeOut();
-      }
+  $(document).scroll(function () {
+      var $nav = $(".fixed-top");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
-  
+});
+
+
+$(function() {
+
+  // contact form animations
+  $('#contact').click(function() {
+    $('#contactForm').fadeToggle();
+  })
+  $(document).mouseup(function (e) {
+    var container = $("#contactForm");
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.fadeOut();
+    }
   });
 
+});
 
 
-  $(document).ready(function(){
-    $('.parallax').parallax();
-  });
+
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
