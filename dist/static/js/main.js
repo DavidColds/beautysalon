08e13dd10048ad7674b2f3da70e2ea3a6f98225e
David Drughi <drughidavid6@gmail.com>
2020-06-05 11:23:22 +0200

    finnal commit

--- dist/static/js/main.js
+++ dist/static/js/main.js
@@ -1,10 +1,10 @@
-$(function() {
+$(function () {
     var selectedClass = "";
-    $(".filter").click(function() {
+    $(".filter").click(function () {
         selectedClass = $(this).attr("data-rel");
         $("#gallery").fadeTo(100, 0.1);
         $("#gallery div").not("." + selectedClass).fadeOut().removeClass('animation');
-        setTimeout(function() {
+        setTimeout(function () {
             $("." + selectedClass).fadeIn().addClass('animation');
             $("#gallery").fadeTo(300, 1);
         }, 300);
